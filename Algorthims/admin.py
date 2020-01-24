78892469ec692daa9a8af00ba980f0d3da3239ac
mohaned mashaly <30902228+12mohaned@users.noreply.github.com>
2020-01-24 18:12:48 +0200

    Add PostAdmin, User Admin

--- Algorthims/admin.py
+++ Algorthims/admin.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Algorthim ,Algorthim_Category
+from .models import Algorthim ,Algorthim_Category, Post
+from .models import users
 class AlgorthimAdmin(admin.ModelAdmin):
     fields = ["Algorthim_id","Algorthim_name","Algorthim_Content","Algorthim_Code","Category"]
 admin.site.register(Algorthim,AlgorthimAdmin)
@@ -10,4 +11,11 @@ admin.site.register(Algorthim,AlgorthimAdmin)
 class CategoryAdmin(admin.ModelAdmin):
     fields = ["Category_name", "Category_description"]
 admin.site.register(Algorthim_Category,CategoryAdmin)
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ["Username","Email","Biography","BirthDate","Profession"]
+admin.site.register(users,UserAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ["Title","User","Content","Tags","published_Date"]
+admin.site.register(Post,PostAdmin)
