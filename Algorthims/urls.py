acf5edcdc8d693913d3511110d07544c697cb257
mohaned mashaly <30902228+12mohaned@users.noreply.github.com>
2020-02-01 15:47:41 +0200

    Update urls.py

--- Algorthims/urls.py
+++ Algorthims/urls.py
@@ -2,7 +2,12 @@ from django.conf.urls import url
 from . import views
 app_name = 'Algorthims'
 urlpatterns = [
-url("Home",views.home,name = "Home"),
-url("SearchingAlgorthims",views.Searching,name = "Searching"),
-url("SearchingAlgorthims/Patternsearching/<str:algo>",views.SearchingAlgorthims,name ="SearchingAlgorthims")
+url("Home",views.Home,name = "Home"),
+url("logout",views.Logout_request,name="logout"),
+url("Category/<str:Category>",views.Categories,name = "Categories"),
+url("SignUp",views.Signup,name = "SignupForm"),
+url("Login",views.Login_request,name = "Login"),
+url("Blog",views.Blog,name = "Blog"),
+url("WritePost",views.Write_Post,name="WritePost"),
+url("Profile",views.profile,name = "Profile")
 ]
