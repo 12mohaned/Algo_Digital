ed299582faae070d5894e45003c5c27ed3b809a7
mohaned mashaly <30902228+12mohaned@users.noreply.github.com>
2020-02-03 03:54:36 +0200

    Create forms.py

---
+++ Algorthims/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Post, users, Comment
+import collections
+
+
+class SignupForm(UserCreationForm):
+    first_name = forms.CharField(max_length = 30,required = False,help_text='Optional.')
+    last_name = forms.CharField(max_length = 30,required = False, help_text='Optional.')
+    email = forms.EmailField(max_length =100,required = True, help_text='Required. Inform a valid email address.')
+    class Meta:
+        model = User
+        fields = ("username","first_name","last_name","email","password1","password2")
+
+class WritePostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ("Title","Content")
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields =("Comment_Content",)
