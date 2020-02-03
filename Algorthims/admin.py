fdaf42ddcd7b30e8ee44043d69174cba7b542a81
mohaned mashaly <30902228+12mohaned@users.noreply.github.com>
2020-02-03 03:53:21 +0200

    Add CommentAdmin

--- Algorthims/admin.py
+++ Algorthims/admin.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
-from .models import Algorthim ,Algorthim_Category, Post
-from .models import users
+from .models import Algorthim ,Algorthim_Category, Post, PostFavorites, users, Comment
+
 class AlgorthimAdmin(admin.ModelAdmin):
     fields = ["Algorthim_id","Algorthim_name","Algorthim_Content","Algorthim_Code","Category"]
 admin.site.register(Algorthim,AlgorthimAdmin)
@@ -19,3 +18,11 @@ admin.site.register(users,UserAdmin)
 class PostAdmin(admin.ModelAdmin):
     fields = ["Title","User","Content","Tags","published_Date"]
 admin.site.register(Post,PostAdmin)
+
+class postFavoritesAdmin(admin.ModelAdmin):
+    fields = ["Favorite_Post","Favorite_user"]
+admin.site.register(PostFavorites,postFavoritesAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    fields=["User_Comment","Post_User","Comment_Content","Post","Comment_Date"]
+admin.site.register(Comment,CommentAdmin)
